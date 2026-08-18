71aa15968c9b11dfa9c73ef4a94a9691de258624
Eduarda <214549781+byeduardaanjos@users.noreply.github.com>
2026-08-17 23:57:30 -0300

    Disponibiliza agendamentos até 17h
    * Libera agendamentos até 17h
    
    * Libera agendamentos até 17h
    
    * Versiona último horário às 17h

--- supabase/migrations/20260817_booking_self_service_availability.sql
+++ supabase/migrations/20260817_booking_self_service_availability.sql
@@ -31,7 +31,6 @@ create or replace function public.booking_slot_is_valid(p_date date,p_time time 
 select p_date between (now() at time zone 'America/Sao_Paulo')::date and ((now() at time zone 'America/Sao_Paulo')::date+90)
 and extract(dow from p_date)<>0
 and p_time in('08:00'::time,'09:00'::time,'10:00'::time,'11:00'::time,'12:00'::time,'13:00'::time,'14:00'::time,'15:00'::time,'16:00'::time,'17:00'::time)
-and ((extract(hour from p_time)::integer*60)+extract(minute from p_time)::integer+p_duration)<=1050
 and (p_date+p_time)>=((now() at time zone 'America/Sao_Paulo')+interval '1 hour');
 $function$;
 
