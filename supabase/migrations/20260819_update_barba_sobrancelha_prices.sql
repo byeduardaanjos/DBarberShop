a1edfdde835500809369d62c3d6ec829c6f1e2d6
Eduarda <214549781+byeduardaanjos@users.noreply.github.com>
2026-08-19 18:14:37 -0300

    Versiona preços individuais e combinados

--- supabase/migrations/20260819_update_barba_sobrancelha_prices.sql
+++ supabase/migrations/20260819_update_barba_sobrancelha_prices.sql
@@ -5,3 +5,22 @@ set price_cents = case name
   else price_cents
 end
 where name in ('Barba', 'Sobrancelha');
+
+update public.services
+set price_cents = case name
+  when 'Tesoura + Barba' then 4500
+  when 'Degradê + Barba' then 5000
+  when 'Navalhado + Barba' then 5500
+  when 'Completo Tesoura' then 5000
+  when 'Degradê + Barba + Sobrancelha' then 5500
+  when 'Degradê Navalhado + Barba + Sobrancelha' then 6000
+  else price_cents
+end
+where name in (
+  'Tesoura + Barba',
+  'Degradê + Barba',
+  'Navalhado + Barba',
+  'Completo Tesoura',
+  'Degradê + Barba + Sobrancelha',
+  'Degradê Navalhado + Barba + Sobrancelha'
+);
